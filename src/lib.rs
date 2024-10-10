e0aece060800100b949334e92cf5f5cb9c3729c9
Yusuke Shibata <yusukeshib@gmail.com>
2024-10-09 21:03:24 -0400

    Recover preview mode

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,77 @@ pub struct Maku {
     filters: Vec<Filter>,
 }
 
+pub enum Target {
+    Screen {
+        context: three_d::Context,
+        width: u32,
+        height: u32,
+    },
+    Pixels {
+        context: three_d::Context,
+        texture: three_d::Texture2D,
+    },
+}
+
+impl Target {
+    pub fn context(&self) -> &three_d::Context {
+        match self {
+            Target::Screen { context, .. } => context,
+            Target::Pixels { context, .. } => context,
+        }
+    }
+    pub fn clear(&mut self, clear_state: three_d::ClearState) -> &Self {
+        match self {
+            Target::Screen {
+                context,
+                width,
+                height,
+            } => {
+                three_d::RenderTarget::screen(context, *width, *height).clear(clear_state);
+            }
+            Target::Pixels { texture, .. } => {
+                texture.as_color_target(None).clear(clear_state);
+            }
+        }
+        self
+    }
+    pub fn write<E: std::error::Error>(
+        &mut self,
+        render: impl FnOnce() -> Result<(), E>,
+    ) -> Result<(), E> {
+        match self {
+            Target::Screen {
+                context,
+                width,
+                height,
+            } => {
+                three_d::RenderTarget::screen(context, *width, *height).write(render)?;
+            }
+            Target::Pixels { texture, .. } => {
+                texture.as_color_target(None).write(render)?;
+            }
+        }
+        Ok(())
+    }
+    pub fn pixels(&mut self) -> Vec<u8> {
+        match self {
+            Target::Pixels { texture, .. } => texture
+                .as_color_target(None)
+                .read::<[u8; 4]>()
+                .into_iter()
+                .flatten()
+                .collect::<Vec<u8>>(),
+            Target::Screen { .. } => unreachable!(),
+        }
+    }
+}
+
 impl Maku {
     pub async fn load(
         context: &three_d::Context,
         json_path: std::path::PathBuf,
     ) -> Result<Maku, MakuError> {
+        log::debug!("Load json: {:?}", json_path);
         let json = std::fs::read_to_string(json_path)?;
         let project = serde_json::from_str::<IoProject>(&json).map_err(MakuError::from)?;
         Maku::load_project(context, project).await
@@ -71,9 +137,11 @@ impl Maku {
         self.camera.viewport().height
     }
 
-    pub fn render(&mut self, context: &three_d::Context) -> Result<(), MakuError> {
+    pub fn render(&mut self, target: &mut Target) -> Result<(), MakuError> {
         let width = self.width() as f32;
         let height = self.height() as f32;
+
+        target.clear(three_d::ClearState::default());
 
         for filter in self.filters.iter() {
             self.output.as_color_target(None).write(|| {
@@ -81,7 +149,7 @@ impl Maku {
                     Filter::Image(texture) => {
                         let model = three_d::Gm::new(
                             three_d::Rectangle::new(
-                                context,
+                                target.context(),
                                 three_d::vec2(width * 0.5, height * 0.5),
                                 three_d::degrees(0.0),
                                 width,
@@ -97,7 +165,7 @@ impl Maku {
                     }
                     Filter::Shader(program) => {
                         let positions = three_d::VertexBuffer::new_with_data(
-                            context,
+                            target.context(),
                             &[
                                 three_d::vec3(-1.0, -1.0, 0.0),
                                 three_d::vec3(-1.0, 1.0, 0.0),
@@ -130,13 +198,13 @@ impl Maku {
             // TODO: Don't call it in render()
             {
                 let copy_program = three_d::Program::from_source(
-                    context,
+                    target.context(),
                     include_str!("./copy.vert"),
                     include_str!("./copy.frag"),
                 )
                 .unwrap();
                 let copy_positions = three_d::VertexBuffer::new_with_data(
-                    context,
+                    target.context(),
                     &[
                         three_d::vec3(-1.0, -1.0, 0.0),
                         three_d::vec3(-1.0, 1.0, 0.0),
@@ -166,35 +234,16 @@ impl Maku {
             }
         }
 
-        Ok(())
-    }
-
-    pub fn save_to_file(
-        &self,
-        context: &three_d::Context,
-        output_path: std::path::PathBuf,
-    ) -> Result<(), MakuError> {
-        let width = self.width() as f32;
-        let height = self.height() as f32;
-
-        let target = three_d::ColorTargetMultisample::<[u8; 4]>::new(
-            context,
-            width as u32,
-            height as u32,
-            4,
-        );
-        target.clear(three_d::ClearState::default());
-
-        // TODO: Dedup
-        target.write(|| {
+        // TODO: Use blit_to
+        {
             let copy_program = three_d::Program::from_source(
-                context,
+                target.context(),
                 include_str!("./copy.vert"),
                 include_str!("./copy.frag"),
             )
             .unwrap();
             let copy_positions = three_d::VertexBuffer::new_with_data(
-                context,
+                target.context(),
                 &[
                     three_d::vec3(-1.0, -1.0, 0.0),
                     three_d::vec3(-1.0, 1.0, 0.0),
@@ -213,21 +262,36 @@ impl Maku {
             );
             copy_program.use_vertex_attribute("position", &copy_positions);
             copy_program.use_texture("u_texture", &self.output);
-            copy_program.draw_arrays(
-                three_d::RenderStates::default(),
-                self.camera.viewport(),
-                copy_positions.vertex_count(),
-            );
-            Ok::<(), MakuError>(())
-        })?;
+            target.write(|| {
+                copy_program.draw_arrays(
+                    three_d::RenderStates::default(),
+                    self.camera.viewport(),
+                    copy_positions.vertex_count(),
+                );
+                Ok::<(), MakuError>(())
+            })?;
+        }
 
-        let mut texture = target.resolve();
-        let pixels: Vec<u8> = texture
-            .as_color_target(None)
-            .read::<[u8; 4]>()
-            .into_iter()
-            .flatten()
-            .collect();
+        Ok(())
+    }
+
+    pub fn render_to_file(
+        &mut self,
+        context: &three_d::Context,
+        output_path: std::path::PathBuf,
+    ) -> Result<(), MakuError> {
+        let width = self.width() as f32;
+        let height = self.height() as f32;
+
+        let texture = new_texture(context, self.width(), self.height());
+        let mut target = Target::Pixels {
+            context: context.clone(),
+            texture,
+        };
+
+        self.render(&mut target)?;
+
+        let pixels = target.pixels();
         image::save_buffer_with_format(
             output_path,
             &pixels,
@@ -236,6 +300,7 @@ impl Maku {
             image::ColorType::Rgba8,
             image::ImageFormat::Png,
         )?;
+
         Ok(())
     }
 }
