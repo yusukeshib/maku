a9188580e0ee31da7dc5fcf32b83a0d55e7900ff
Yusuke Shibata <yusukeshib@gmail.com>
2025-01-28 07:43:47 -0500

    Improve NumberInput

--- apps/editor/src/NumberInput.tsx
+++ apps/editor/src/NumberInput.tsx
@@ -1,10 +1,15 @@
-import { type ChangeEvent, useEffect, useRef, useState } from 'react';
+import { type KeyboardEvent, type ChangeEvent, useEffect, useRef, useState } from 'react';
 import css from './NumberInput.module.css'
 
+const precision = 2;
+const base = Math.pow(10, precision);
+
 function format(v:number): string {
-  const prime = Math.floor(v);
-  const frac = Math.round((v - prime) * 100) / 100;
-  return `${prime}.${frac}`;
+  const negative = v < 0;
+  const p = Math.abs(v);
+  const prime = Math.floor(p);
+  const frac = `${Math.round((p - prime) * base)}`.padStart(precision, '0')
+  return `${negative ? '-' : ''}${prime}.${frac}`;
 }
 
 function parse(str: string): number {
@@ -28,27 +33,32 @@ export function NumberInput({ value, onChange }: { value: number; onChange: (val
     }
   }
 
-
-  const ref = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    const elem = ref.current;
-    if(!elem) return;
-
-    function handleKeyDown(evt: KeyboardEvent) {
-      if(evt.code === 'Enter' || evt.code === 'Escape') {
-        elem?.blur();
+  function handleKeyDown(evt: KeyboardEvent<HTMLInputElement>) {
+    switch(evt.code) {
+    case 'Enter':
+    case 'Escape':
+      {
+        evt.currentTarget.blur();
       }
-    }
-
-    elem.addEventListener('keydown', handleKeyDown)
-    return () => {
-      elem.removeEventListener('keydown', handleKeyDown)
-    }
-  }, []);
-
+      break;
+    case 'ArrowUp':
+      {
+        const newValue = value + (evt.shiftKey ? 10 : 1)/base;
+        setInternalValue(format(newValue))
+        onChange(newValue);
+      }
+      break
+    case 'ArrowDown':
+      {
+        const newValue = value - (evt.shiftKey ? 10 : 1)/base;
+        setInternalValue(format(newValue))
+        onChange(newValue);
+      }
+      break
+    } 
+  }
 
   return (
-    <input ref={ref} onBlur={handleBlur} className={css.input} type='text' value={internalValue} onChange={handleChange} />
+    <input onKeyDown={handleKeyDown} onBlur={handleBlur} className={css.input} type='text' value={internalValue} onChange={handleChange} />
   )
 }
