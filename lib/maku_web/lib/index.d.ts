0c1d529f96902fcdd92ecf99136c6eb90243bb5d
Yusuke Shibata <yusukeshib@gmail.com>
2025-11-17 21:31:13 -0500

    built

---
+++ lib/maku_web/lib/index.d.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * ---------- Engine exposed to WASM ----------
+ */
+export class WasmEngine {
+  free(): void;
+  [Symbol.dispose](): void;
+  constructor();
+  /**
+   * graph: JsGraph を表す JS オブジェクト
+   * inputs: { [valueId: string]: JsTensor } な JS オブジェクト
+   *
+   * 返り値: { [valueId: string]: JsTensor } な JS オブジェクト
+   */
+  run(graph: any, inputs: any): any;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_wasmengine_free: (a: number, b: number) => void;
+  readonly wasmengine_new: () => number;
+  readonly wasmengine_run: (a: number, b: any, c: any) => [number, number, number];
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_externrefs: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
