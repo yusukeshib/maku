f749b62c87c1e6d47d3f0d1ee61cd18f6d9babd3
Yusuke Shibata <yusukeshib@gmail.com>
2025-11-17 21:47:20 -0500

    Cleanup

--- apps/editor/src/App.tsx
+++ apps/editor/src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { runExample } from './test'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
 
   const handleClick = () => {
     setCount((count) => count + 1)
@@ -14,14 +11,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleClick}>
